6247c8dddfc69232d284ff00cbd80b2ee4571a8a
yoppedayo8545 <yoppedayo8545@gmail.com>
2021-06-26 14:32:49 +0900

    微修正
    日本時間にするとエラーが起きてサーバーが起動しないので一旦コメントアウト。
    ついでにインデント修正。

--- config/application.rb
+++ config/application.rb
@@ -8,10 +8,11 @@ module Baukis2
   class Application < Rails::Application
     config.load_defaults 6.0
 
-    config.time_zone = "tokyo"
-    config.i18n.load_path +=
-    Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}").to_s]
-    config.i18n.default_locale = :ja
+    # config.time_zone = "tokyo"
+    # config.active_record.default_timezone = :local
+      config.i18n.load_path +=
+        Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}").to_s]
+      config.i18n.default_locale = :ja
 
     config.generators do |g|
       g.skip_routes true
