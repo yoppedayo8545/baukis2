d3aea540f25b92479f696b16c2890456b5c31177
yoppedayo8545 <yoppedayo8545@gmail.com>
2021-07-11 14:30:49 +0900

    404errorの補足用

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < ActionController::Base
   rescue_from StandardError, with: :rescue500
   rescue_from Forbidden, with: :rescue403
   rescue_from IpAddressRejected, with: :rescue403
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue404
 
   private def set_layout
     if params[:controller].match(%r{\A(staff|admin|customer)/})
@@ -21,6 +22,10 @@ class ApplicationController < ActionController::Base
     render "errors/forbidden", status: 403 
   end
 
+  private def rescue404(e)
+    render "errors/not_found", status: 404
+  end
+    
   private def rescue500(e)
     render "errors/internal_server_error", status: 500
   end
