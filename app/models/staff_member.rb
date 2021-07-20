a9baf64a6967b1f586e661089ea0856123305486
yoppedayo8545 <yoppedayo8545@gmail.com>
2021-07-20 20:41:48 +0900

    passwordのハッシュ化

--- app/models/staff_member.rb
+++ app/models/staff_member.rb
@@ -1,2 +1,9 @@
 class StaffMember < ApplicationRecord
+  def password=(raw_password)
+    if raw_password.kind_of?(string)
+      self.hashed_password = Bcrypt::Password.create(raw_password)
+    elsif raw_password.nil?
+      self.hashed_password = nil
+    end
+  end
 end
