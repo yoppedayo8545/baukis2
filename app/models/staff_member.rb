d1819bf24e1b119d2baaba21043ebbca340a78eb
yoppedayo8545 <yoppedayo8545@gmail.com>
2021-07-29 18:49:59 +0900

    スペルミス修正

--- app/models/staff_member.rb
+++ app/models/staff_member.rb
@@ -1,7 +1,7 @@
 class StaffMember < ApplicationRecord
   def password=(raw_password)
     if raw_password.kind_of?(String)
-      self.hashed_password = Bcrypt::Password.create(raw_password)
+      self.hashed_password = BCrypt::Password.create(raw_password)
     elsif raw_password.nil?
       self.hashed_password = nil
     end
