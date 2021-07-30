06074e0c847cc59f2956e2a574cb7c8ef7d85eef
yoppedayo8545 <yoppedayo8545@gmail.com>
2021-07-30 12:50:21 +0900

    モデル作成に伴い作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_11_061724) do
+ActiveRecord::Schema.define(version: 2021_07_30_020300) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "administrators", force: :cascade do |t|
+    t.string "email"
+    t.string "hashed_password"
+    t.boolean "suspended", default: false, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index "lower((email)::text)", name: "index_administrators_on_LOWER_email", unique: true
+  end
 
   create_table "staff_members", force: :cascade do |t|
     t.string "email", null: false
