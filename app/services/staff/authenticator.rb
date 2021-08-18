c94943e77b6989ec59ed8e1d0a638aa0779b1e82
yoppedayo8545 <yoppedayo8545@gmail.com>
2021-08-18 17:37:58 +0900

    エラー解消のため一部コメントアウト
    マイグレーション時にエラーとなったカラム関連の記述削除

--- app/services/staff/authenticator.rb
+++ app/services/staff/authenticator.rb
@@ -5,10 +5,10 @@ class Staff::Authenticator
 
   def authenticate(raw_password)
     @staff_member &&
-      !@staff_member.suspended? &&
+      # !@staff_member.suspended? &&
       @staff_member.hashed_password &&
-      @staff_member.start_date <= Date.today &&
-      (@staff_member.end_date.nil? || @staff_member.end_date > Date.today) &&
+      # @staff_member.start_date <= Date.today &&
+      # (@staff_member.end_date.nil? || @staff_member.end_date > Date.today) &&
       BCrypt::Password.new(@staff_member.hashed_password) == raw_password
   end
 end
