f87083afd3bc66ecc6e155a43b6a55450c1d35c4
yoppedayo8545 <yoppedayo8545@gmail.com>
2021-08-20 17:58:35 +0900

    記述ミスがあったため修正
    suspended?の判定を削除し、hashed_passwordの判定を追加。

--- app/services/staff/authenticator.rb
+++ app/services/staff/authenticator.rb
@@ -5,9 +5,9 @@ class Staff::Authenticator
 
   def authenticate(raw_password)
     @staff_member &&
-      # !@staff_member.suspended? &&
-      # @staff_member.start_date <= Date.today &&
-      # (@staff_member.end_date.nil? || @staff_member.end_date > Date.today) &&
+      @staff_member.hashed_password &&
+      @staff_member.start_date <= Date.today &&
+      (@staff_member.end_date.nil? || @staff_member.end_date > Date.today) &&
       BCrypt::Password.new(@staff_member.hashed_password) == raw_password
   end
 end
