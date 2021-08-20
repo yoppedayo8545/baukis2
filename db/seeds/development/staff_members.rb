394bf98cb793b328a518a911e4cb6ab29caaf7cf
yoppedayo8545 <yoppedayo8545@gmail.com>
2021-08-20 17:42:12 +0900

    カラム追加に伴う修正

--- db/seeds/development/staff_members.rb
+++ db/seeds/development/staff_members.rb
@@ -4,6 +4,6 @@ StaffMember.create!(
   given_name: "太郎",
   family_name_kana: "ヤマダ",
   given_name_kana: "タロウ",
-  password: "password"
-  # start_date: Date.today
+  password: "password",
+  start_date: Date.today
 )
