19b10d44d8b1f9bae88648d51acd3abfa9052ca7
yoppedayo8545 <yoppedayo8545@gmail.com>
2021-08-20 19:09:32 +0900

    adminログインテスト

---
+++ spec/services/admin/authenticator_spec.rb
@@ -0,0 +1,20 @@
+require "rails_helper"
+
+describe Admin::Authenticator do
+  describe "#authenticate" do
+    example "正しいパスワードならtrueを返す" do
+      m = build(:administrator)
+      expect(Admin::Authenticator.new(m).authenticate("pw")).to be_truthy
+    end
+    
+    example "謝ったパスワードならfalseを返す" do
+      m = build(:administrator)
+      expect(Admin::Authenticator.new(m).authenticate("xy")).to be_falsey
+    end
+
+    example "パスワード未設定ならfalseを返す" do
+      m = build(:administrator, password: nil)
+      expect(Admin::Authenticator.new(m).authenticate(nil)).to be_falsey
+    end
+  end
+end
