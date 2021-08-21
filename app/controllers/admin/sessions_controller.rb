d5bfe333989bf7676afde1ad7953176f4d99535a
yoppedayo8545 <yoppedayo8545@gmail.com>
2021-08-21 18:14:06 +0900

    誤表記の修正
    モデル名ではなくテーブル名の大文字だった

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -12,7 +12,7 @@ class Admin::SessionsController < Admin::Base
     @form = Admin::LoginForm.new(params[:admin_login_form])
     if @form.email.present?
       administrator =
-        Admin.find_by("LOWER(email) = ?", @form.email.downcase)
+        Administrator.find_by("LOWER(email) = ?", @form.email.downcase)
     end
     if Admin::Authenticator.new(administrator).authenticate(@form.password)
       if administrator.suspended?
