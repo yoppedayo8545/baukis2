4e22bfe6d351d42439943ded998a2faedea29fd9
yoppedayo8545 <yoppedayo8545@gmail.com>
2021-08-25 15:50:59 +0900

    strong_parameter設定

--- app/controllers/staff/sessions_controller.rb
+++ app/controllers/staff/sessions_controller.rb
@@ -9,7 +9,7 @@ class Staff::SessionsController < Staff::Base
   end
 
   def create
-    @form = Staff::LoginForm.new(params[:staff_login_form])
+    @form = Staff::LoginForm.new(login_form_params)
     if @form.email.present?
       staff_member =
         StaffMember.find_by("LOWER(email) = ?", @form.email.downcase)
@@ -29,6 +29,11 @@ class Staff::SessionsController < Staff::Base
     end
   end
 
+  private 
+  def login_form_params
+    params.require(:admin_login_form).permit(:email, :paddword)
+  end
+  
   def destroy
     session.delete(:staff_member_id)
     flash.notice = "ログアウトしました。"
