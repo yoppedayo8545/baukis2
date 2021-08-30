eb932ee78295d344113d9891ab084a4578536c63
yoppedayo8545 <yoppedayo8545@gmail.com>
2021-08-30 16:19:20 +0900

    不要なコードの修正
    staffに記述するコードだった

--- app/controllers/admin/base.rb
+++ app/controllers/admin/base.rb
@@ -1,7 +1,5 @@
 class Admin::Base < ApplicationController
   before_action :authorize
-  before_action :check_account
-  before_action :check_timeout
 
   private def current_administrator
     if session[:administrator_id]
@@ -17,29 +15,6 @@ class Admin::Base < ApplicationController
     unless current_administrator
       flash.alert = "管理者としてログインしてください。"
       redirect_to :admin_login
-    end
-  end
-
-  def check_account
-    if current_staff_member && !current_staff_member.active?
-      session.delete(:staff_member_id)
-      flash.alert = "アカウントが無効になりました。"
-      redirect_to :staff_root
-    end
-  end
-
-  TIMEOUT = 60.minutes
-
-  private
-  def check_timeout
-    if current_staff_member
-      if session[:last_access_time] >= TIMEOUT.ago
-        session[:last_access_time] >= Time.current
-      else
-        session.delete(:staff_member_id)
-        flash.alert = "セッションがタイムアウトしました。"
-        redirect_to :staff_login
-      end
     end
   end
 end
