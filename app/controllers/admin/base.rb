12c32d2af1c72f87f04ab9e8b69659c17a4dd630
yoppedayo8545 <yoppedayo8545@gmail.com>
2021-08-30 14:59:45 +0900

    before_actionの追加

--- app/controllers/admin/base.rb
+++ app/controllers/admin/base.rb
@@ -1,4 +1,8 @@
 class Admin::Base < ApplicationController
+  before_action :authorize
+  before_action :check_account
+  before_action :check_timeout
+
   private def current_administrator
     if session[:administrator_id]
       @current_administrator ||=
@@ -7,4 +11,35 @@ class Admin::Base < ApplicationController
   end
 
   helper_method :current_administrator
+
+  private 
+  def authorize
+    unless current_administrator
+      flash.alert = "管理者としてログインしてください。"
+      redirect_to :admin_login
+    end
+  end
+
+  def check_account
+    if current_staff_member && !current_staff_member.active?
+      session.delete(:staff_member_id)
+      flash.alert = "アカウントが無効になりました。"
+      redirect_to :staff_root
+    end
+  end
+
+  TIMEOUT = 60.minutes
+
+  private
+  def check_timeout
+    if current_staff_member
+      if session[:last_access_time] >= TIMEOUT.ago
+        session[:last_access_time] >= Time.current
+      else
+        session.delete(:staff_member_id)
+        flash.alert = "セッションがタイムアウトしました。"
+        redirect_to :staff_login
+      end
+    end
+  end
 end
