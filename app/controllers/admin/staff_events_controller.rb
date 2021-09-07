f442204bf8ca8c51773930a3491f3e0813a58191
yoppedayo8545 <yoppedayo8545@gmail.com>
2021-09-07 12:37:39 +0900

    リファクタリング

--- app/controllers/admin/staff_events_controller.rb
+++ app/controllers/admin/staff_events_controller.rb
@@ -2,9 +2,11 @@ class Admin::StaffEventsController < Admin::Base
   def index
     if params[:staff_member_id]
       @staff_member = StaffMember.find(params[:staff_member_id])
-      @events = @staff_member.events.order(occurred_at: :desc)
+      @events = @staff_member.events
     else
       @events =StaffEvent.order(occurred_at: :desc)
     end
+      @events = 
+        @events.order(occurred_at: :desc).includes(:member).page(params[:page])
   end
 end
