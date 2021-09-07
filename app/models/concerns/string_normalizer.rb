a727b8444e6eb182256296025a296b4b085d89aa
yoppedayo8545 <yoppedayo8545@gmail.com>
2021-09-07 16:31:56 +0900

    emailの全角を半角へ変更機能追加

--- app/models/concerns/string_normalizer.rb
+++ app/models/concerns/string_normalizer.rb
@@ -3,6 +3,10 @@ require "nkf"
 module StringNormalizer
   extend ActiveSupport::Concern
 
+  def normalize_as_email(text)
+    NKF.nkf("-W -w -Z1", text).strip if text
+  end
+
   def normalize_as_name(text)
     NKF.nkf("-W -w -Z1", text).strip if text
   end
@@ -10,4 +14,4 @@ module StringNormalizer
   def normalize_as_furigana(text)
     NKF.nkf("-W -w -Z1 --katakana", text).strip if text
   end
-end
+end 
