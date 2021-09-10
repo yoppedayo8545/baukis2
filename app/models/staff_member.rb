061fedf1ae1fb791fe9ced9bb71e1ac04763abc0
yoppedayo8545 <yoppedayo8545@gmail.com>
2021-09-10 12:51:38 +0900

    氏名のvalidation追加
    ※漢字・ひらがな・カタカナ・アルファベットのみOK

--- app/models/staff_member.rb
+++ app/models/staff_member.rb
@@ -12,10 +12,12 @@ class StaffMember < ApplicationRecord
   end
 
   KATAKANA_REGEXP = /\A[\p{katakana}\u{30fc}]+\z/
+  HUMAN_NAME_REGEXP = /\A[\p{han}\p{hiragana}\p{katakana}\u{30fc}A-Za-z]+\z/
 
   validates :email, presence: true, "valid_email_2/email": true,
     uniqueness: { case_sensitive: false }
-  validates :family_name, :given_name, presence: true
+  validates :family_name, :given_name, presence: true,
+  format: { with: HUMAN_NAME_REGEXP, allow_blank: true }
   validates :family_name_kana, :given_name_kana, presence: true,
   format: { with: KATAKANA_REGEXP, allow_blank: true }
   validates :start_date, presence: true, date: {
