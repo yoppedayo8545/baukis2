6c84491d148df3061cc0fca40237e91bc122939e
yoppedayo8545 <yoppedayo8545@gmail.com>
2021-09-10 12:49:53 +0900

    passwordのvalidation追加

--- app/forms/staff/change_password_form.rb
+++ app/forms/staff/change_password_form.rb
@@ -3,9 +3,18 @@ class Staff::ChangePasswordForm
 
   attr_accessor :object, :current_password, :new_password,
     :new_password_confirmation
+  validates :new_password, presence: true, confirmation: true
+
+  validate do
+    unless Staff::Authenticator.new(object).authenticate(current_password)
+      errors.add(:current_password, :wrong)
+    end
+  end
 
   def save
-    object.password = new_password
-    object.save!
+    if valid?
+      object.password = new_password
+      object.save!
+    end
   end
 end
