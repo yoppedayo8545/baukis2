e7bc9558b8c6cf051ae11e85a322c6e467421f65
yoppedayo8545 <yoppedayo8545@gmail.com>
2021-09-15 15:29:48 +0900

    誤字修正

--- app/controllers/staff/customers_controller.rb
+++ app/controllers/staff/customers_controller.rb
@@ -35,7 +35,7 @@ class Staff::CustomersController < Staff::Base
       flash.notice = "顧客情報を更新しました"
       redirect_to action: "index"
     else
-      flash..now.alert = "入力に誤りがあります"
+      flash.now.alert = "入力に誤りがあります"
       render action: "edit"
     end
   end
