05ffa4991c01161778e284e282d8ca29bb6d556b
yoppedayo8545 <yoppedayo8545@gmail.com>
2021-09-15 16:29:25 +0900

    自宅住所と勤務先住所のチェックボックス用設定

---
+++ app/javascript/staff/customer_form.js
@@ -0,0 +1,26 @@
+const { $ } = require("@rails/ujs");
+
+function toggle_home_address_fields() {
+  const checked = 
+  $("input#form_inputs_home_address").prop("checked");
+  $("fieldset#home_address_fields input").prop("disabled", !checked);
+  $("fieldset#home_address_fields select").prop("disabled", !checked);
+}
+
+function toggle_work_address_fields() {
+  const checked = 
+  $("input#form_inputs_work_address").prop("checked");
+  $("fieldset#work_address_fields input").prop("disabled", !checked);
+  $("fieldset#work_address_fields select").prop("disabled", !checked);
+}
+
+$(document).on("ready turblinks:load", () => {
+  toggle_home_address_fields();
+  toggle_work_address_fields();
+  $("input#form_inputs_home_address").on("click", () => {
+    toggle_home_address_fields();
+  });
+  $("input#form_inputs_work_address").on("click", () => {
+    toggle_work_address_fields();
+  });
+});
