e8656d5f0ef6462ceb905079b6afdf09ea5dff72
yoppedayo8545 <yoppedayo8545@gmail.com>
2021-09-15 15:31:46 +0900

    moduleのincludeとそれに伴う削除

--- app/models/administrator.rb
+++ app/models/administrator.rb
@@ -1,12 +1,7 @@
 class Administrator < ApplicationRecord
-  def password=(raw_password)
-    if raw_password.kind_of?(String)
-      self.hashed_password = BCrypt::Password.create(raw_password)
-    elsif raw_password.nil?
-      self.hashed_password = nil
-    end
-  end
-
+  include EmailHolder
+  include PasswordHolder
+  
   def active?
     !suspended?
   end
