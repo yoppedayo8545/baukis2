f63a4febcb56084f055c7f5ec92d5a464fdd3a46
yoppedayo8545 <yoppedayo8545@gmail.com>
2021-09-28 11:48:38 +0900

    電話番号入力フォームのチェック生成

--- app/javascript/staff/customer_form.js
+++ app/javascript/staff/customer_form.js
@@ -5,6 +5,7 @@ function toggle_home_address_fields() {
   $("input#form_inputs_home_address").prop("checked");
   $("fieldset#home_address_fields input").prop("disabled", !checked);
   $("fieldset#home_address_fields select").prop("disabled", !checked);
+  $("fieldset#home_address_fields").toggle(checked);
 }
 
 function toggle_work_address_fields() {
@@ -12,6 +13,7 @@ function toggle_work_address_fields() {
   $("input#form_inputs_work_address").prop("checked");
   $("fieldset#work_address_fields input").prop("disabled", !checked);
   $("fieldset#work_address_fields select").prop("disabled", !checked);
+  $("fieldset#work_address_fields").toggle(checked);
 }
 
 $(document).on("ready turblinks:load", () => {
